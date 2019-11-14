29d6808b1a795b0b67bcf138daf44e8489d08526
Samrath Matta <samrathsm@icloud.coom>
2019-11-14 10:54:37 -0800

    Add new functionality complete

--- Checklist App/TodoListViewController.swift
+++ Checklist App/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Buy Eggs", "Buy Milk ", "Buy Potatoes"]
+    var itemArray = ["Buy Eggs", "Buy Milk ", "Buy Potatoes"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,9 +42,40 @@ class TodoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         
-        tableView.deselectRow(at:indexPath, animated:true)
+        tableView.deselectRow(at: indexPath, animated: true)
+
     }
     
+    // MARK - Add New Items
 
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Task", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Task", style: .default) { (action) in
+            
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            // What will happen once the useer clicks on the Add Item Buttton on our UIAlert.
+            // print(textField.text)
+            // print("Success!")
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+           // print(alertTextField.placeholder)
+           // print(alertTextField.text)
+            // print("Now")
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
 }
 
